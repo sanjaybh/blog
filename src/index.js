6d67f92f7d7b92b94d5369d70959625dbe0da0b7
sanjaybh <sanjay.bhan@incedoinc.com>
2019-01-21 10:35:33 +0530

    blog changes
    Signed-off-by: sanjaybh <sanjay.bhan@incedoinc.com>

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom"
 
 const App = () => {
- return (<h1>Hello Blog</h1>)
+ return (<h1>Testing Blog application</h1>)
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'))
