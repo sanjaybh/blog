1d97d2e05fb8f6624b5a42296caa8779546a53de
sanjaybh <bhancapri78@yahoo.com>
2019-01-21 10:37:31 +0530

    changes from server as well

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom"
 
 const App = () => {
- return (<h1>Testing Blog application</h1>)
+ return (<h1>Testing Blog application, some changes from server as well</h1>)
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'))
